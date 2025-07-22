9226e0fa005ed42743d7ba28abdb84e98347615c
AadityaAgni <aadityaagni@gmail.com>
2025-07-22 20:43:31 +0000

    proper init

--- __init__.py
+++ __init__.py
@@ -1,28 +1,35 @@
-# """
-# YOLOv9: Learning What You Want to Learn Using Programmable Gradient Information
-# """
+"""
+YOLOv9: Learning What You Want to Learn Using Programmable Gradient Information
+"""
 
-# __version__ = "0.0.1"
+__version__ = "0.0.1"
 
-# # Import key modules for easy access
-# try:
-#     from . import utils
-#     from . import models
-#     from . import detect
-#     from . import train
-#     from . import val
-#     from . import export
-# except ImportError:
-#     # Handle cases where modules might not be available
-#     pass
+# Import submodules to make them accessible as yolov9.utils, yolov9.models, etc.
+from . import utils
+from . import models
 
-# # Make commonly used functions easily accessible
-# def load_model(weights_path, device='cpu'):
-#     """Load a YOLOv9 model from weights file"""
-#     from . import hubconf
-#     return hubconf.custom(weights_path, device=device)
+# Import main scripts as submodules
+from . import detect
+from . import train
+from . import val
+from . import export
+from . import hubconf
 
-# def detect_image(model, image_path, save_dir='runs/detect'):
-#     """Run detection on an image"""
-#     from . import detect
-#     return detect.run(weights=model, source=image_path, project=save_dir)
+# Also import other main modules
+try:
+    from . import detect_dual
+    from . import train_dual
+    from . import train_triple
+    from . import val_dual
+    from . import val_triple
+    from . import benchmarks
+except ImportError:
+    pass
+
+# Import subpackages
+try:
+    from . import classify
+    from . import segment
+    from . import panoptic
+except ImportError:
+    pass
